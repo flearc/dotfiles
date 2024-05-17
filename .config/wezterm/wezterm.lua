7f94da33e54a35b5462062bc06407740541c3625
flea <fleashiro@gmail.com>
2024-05-17 14:28:31 +0800

    add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
+config.font_size = 20
+
+-- no padding
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- tab bar
+config.use_fancy_tab_bar = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+
+return config
